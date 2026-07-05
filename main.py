ec199dd99413f6747e81b87c0dbb4be59b24af53
Cristi5-47 <cr0877957@gmail.com>
2026-07-05 14:04:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer(
+        "👋 Assalomu alaykum!\n\n"
+        "SMM Maker Robotga xush kelibsiz! 🚀"
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
